92141798e73d9dcf990ecf47ed548c1e547c444f
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2023-05-17 02:26:23 +0300

    splash screen

--- dsa/App.tsx
+++ dsa/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NativeBaseProvider, extendTheme } from 'native-base';
@@ -8,6 +8,7 @@ import SignUpScreen from './src/screens/SignUpScreen';
 import LoginStackNavigator from './src/navigation/AppNavigator';
 import Prices from './src/screens/Prices';
 import NewsGeneral from './src/screens/NewsGeneral';
+import EntryScreen from './src/screens/EntryScreen';
 
 
 const theme = extendTheme({});
@@ -15,29 +16,30 @@ const theme = extendTheme({});
 const Stack = createStackNavigator();
 
 const App = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showEntryScreen, setShowEntryScreen] = useState(true);
 
-  // const handleAuthentication = (status: boolean) => {
-  //   setIsLoggedIn(status);
-  // };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowEntryScreen(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <NativeBaseProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="AppNavigator" component={AppNavigator} />
-
-          {/* {isLoggedIn ? (
+        {showEntryScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="EntryScreen" component={EntryScreen} />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="AppNavigator" component={AppNavigator} />
-          ) : (
-            <Stack.Screen name="LoginStackNavigator">
-              {(props) => <SignInScreen {...props} handleAuthentication={handleAuthentication} />}
-            </Stack.Screen>
-          )} */}
-        </Stack.Navigator>
+          </Stack.Navigator>
+        )}
       </NavigationContainer>
-  </NativeBaseProvider>
-    // <Prices />
+    </NativeBaseProvider>
   );
 };
 
